0d93124280e443829e6c863552fe0f05ff570ec0
Tymofiy Bortnyk <tbortnyk@softserveinc.com>
2018-09-25 19:05:41 +0300

    test

--- test/terraform_aws_example_test.go
+++ test/terraform_aws_example_test.go
@@ -41,10 +41,10 @@ func TestTerraformAwsExample(t *testing.T) {
 	// Run `terraform output` to get the value of an output variable
 	instanceID := terraform.Output(t, terraformOptions, "instance_id")
 
-	aws.AddTagsToResource(t, awsRegion, instanceID, map[string]string{"testing": "testing-tag-value"})
+	// aws.AddTagsToResource(t, awsRegion, instanceID, map[string]string{"testing": "testing-tag-value"})
 
 	// Look up the tags for the given Instance ID
-	instanceTags := aws.GetTagsForEc2Instance(t, awsRegion, instanceID)
+	// instanceTags := aws.GetTagsForEc2Instance(t, awsRegion, instanceID)
 
 	testingTag, containsTestingTag := instanceTags["testing"]
 	assert.True(t, containsTestingTag)
