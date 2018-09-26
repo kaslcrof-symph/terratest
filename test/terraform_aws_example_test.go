4e3a2577ef29bd2fad4b01f6962312ac4232f4a7
Tymofiy Bortnyk <tbortnyk@softserveinc.com>
2018-09-26 15:26:24 +0300

    region

--- test/terraform_aws_example_test.go
+++ test/terraform_aws_example_test.go
@@ -28,7 +28,7 @@ func TestTerraformAwsExample(t *testing.T) {
 		// Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
 			"instance_name": expectedName,
-		//	"aws_region":    awsRegion,
+			"aws_region":    awsRegion,
 		},
 
 		// Environment variables to set when running Terraform
